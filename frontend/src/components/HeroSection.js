5091d69b128285d7c559903e995aad4d9cc1a4a6
AMA1120 <115350888+AMA1120@users.noreply.github.com>
2024-04-24 23:20:37 +0530

    Update image paths in HeroSection.css and HeroSection.js

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -5,7 +5,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-       <image src="D:\React\Final Project\frontend\public\Images\agri-home.jpg" />
+       <image src="/src/assets/agri-home.jpg" />
       <h1>GREEN-GROW</h1>
       <h4>For Your Tomorrow's Agriculture</h4>
       <p>Agri Connect & Fertilizer Distribution Tracking Platform</p>
