5612ea2f175378c150250a80eb27f3ced6e04e0b
AMA1120 <115350888+AMA1120@users.noreply.github.com>
2024-04-28 23:29:04 +0530

    Refactor database connection logic in conn.js to use dynamic database name

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -3,9 +3,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connectDB = async () => {
+  const databaseName = process.env.NODE_ENV === "dev" ? "green-grow-dev" : "green-grow-test"
   try {
+    console.log(databaseName)
+
     await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "green-grow-dev",
+      dbName: databaseName,
     });
     console.log("Database Connected");
   } catch (error) {
